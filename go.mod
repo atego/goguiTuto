79b6c4908f3e6984df6aa5b581f114a8740772f6
ilde <atego917@gmail.com>
2026-07-02 09:55:31 +0200

    Inicio

---
+++ go.mod
@@ -0,0 +1,24 @@
+module goguituto
+
+go 1.26.4
+
+require (
+	github.com/go-gui-org/go-glyph v1.12.0
+	github.com/go-gui-org/go-gui v0.29.0
+)
+
+require (
+	github.com/alecthomas/chroma/v2 v2.27.0 // indirect
+	github.com/dlclark/regexp2/v2 v2.2.2 // indirect
+	github.com/go-gl/gl v0.0.0-20260331235117-4566fea9a276 // indirect
+	github.com/go-gui-org/go-glyph/backend/sdl2 v1.11.0 // indirect
+	github.com/go-pdf/fpdf v0.9.0 // indirect
+	github.com/godbus/dbus/v5 v5.2.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/tdewolff/parse/v2 v2.8.13 // indirect
+	github.com/veandco/go-sdl2 v0.4.40 // indirect
+	github.com/yuin/goldmark v1.8.2 // indirect
+	github.com/yuin/goldmark-emoji v1.0.6 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"github.com/go-gui-org/go-glyph"
+	"github.com/go-gui-org/go-gui/gui"
+	"github.com/go-gui-org/go-gui/gui/backend"
+)
+
+type App struct {
+	Nombre     string
+	Mensaje    string
+	ColorTexto gui.Color
+}
+
+func main() {
+	gui.SetTheme(gui.ThemeDark.WithBorders(true))
+
+	w := gui.NewWindow(gui.WindowCfg{
+		State:  &App{},
+		Title:  "Pruebas",
+		Width:  400,
+		Height: 300,
+		OnInit: func(w *gui.Window) {
+			w.UpdateView(mainView)
+		},
+	})
+	gui.State[App](w).Mensaje = "Hola majete!!"
+	gui.State[App](w).ColorTexto = gui.ColorFromString("#ff9f00")
+
+	backend.Run(w)
+}
+
+func mainView(w *gui.Window) gui.View {
+	ww, _ := w.WindowSize()
+	app := gui.State[App](w)
+
+	return gui.Column(gui.ContainerCfg{
+		Width:  float32(ww),
+		Height: 300,
+		Sizing: gui.FillFit,
+		HAlign: gui.HAlignCenter,
+		VAlign: gui.VAlignTop,
+		Content: []gui.View{
+			gui.Input(gui.InputCfg{
+				ID:          "Mensaje",
+				IDFocus:     1,
+				Sizing:      gui.FillFit,
+				Placeholder: "Mensaje",
+				Text:        app.Nombre,
+				OnTextChanged: func(l *gui.Layout, s string, w *gui.Window) {
+					app.Nombre = s
+				},
+			}),
+			gui.Text(gui.TextCfg{
+				Text: app.Mensaje,
+				TextStyle: gui.TextStyle{
+					Size:     24,
+					Color:    app.ColorTexto,
+					Typeface: glyph.TypefaceBold,
+					Align:    gui.TextAlignCenter,
+				},
+				Mode: gui.TextModeWrap,
+			}),
+			gui.Button(gui.ButtonCfg{
+				Content: []gui.View{
+					gui.Text(gui.TextCfg{Text: "Cambiar texto"}),
+				},
+				OnClick: func(l *gui.Layout, e *gui.Event, w *gui.Window) {
+					app.Mensaje = "Eres un puto gilipollas😂, " + app.Nombre
+					app.ColorTexto = gui.ColorFromString("#48ff00")
+					app.Nombre = ""
+					e.IsHandled = true
+				},
+			}),
+		},
+	})
+}
